863e193ac04bca8c8f959acb19c5e7287deaf279
AndrewPamer <AndrewPamer@my.unt.edu>
2023-02-23 20:03:02 -0600

    Added Button Functionality

---
+++ eth.js
@@ -0,0 +1,10 @@
+const ethButton = document.querySelector('connect-button');
+if(typeof window.ethereum !== 'undefined') {
+  ethButton.addEventListener('click', () => {
+    ethereum.request({ method: 'eth_requestAccounts' });
+  });
+
+}
+else {
+  console.log('Install MetaMask!');
+}
