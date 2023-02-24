7b9ae53707aa639e386c69b9f0b32dd1a17841d5
AndrewPamer <AndrewPamer@my.unt.edu>
2023-02-23 21:43:54 -0600

    Added Buttons

--- eth.js
+++ eth.js
@@ -1,11 +1,29 @@
 
-const ethButton = document.querySelector('connect-button');
+//Sign in button
+const ethButton = document.getElementById('connect-button');
+
+//Show the signed in account
+const showAccount = document.getElementById('eth-account');
+
+//Check that Metamask is installed
 if(typeof window.ethereum !== 'undefined') {
   ethButton.addEventListener('click', () => {
-    ethereum.request({ method: 'eth_requestAccounts' });
+    getAccount();
   });
-
 }
 else {
+  //If metamask isn't installed - tell the user to install metamask
+  showAccount.innerHTML = "Please Install MetaMask!";
   console.log('Install MetaMask!');
+}
+
+async function getAccount() {
+  //Get account (as an array)
+  const accounts = await ethereum.request ({method: 'eth_requestAccounts'});
+
+  //Get the first element of accounts
+  const account = accounts[0];
+
+  //Change HTML element
+  showAccount.innerHTML =  "Account: "+account;
 }
